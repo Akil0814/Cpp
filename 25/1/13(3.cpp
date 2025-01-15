22dc144941dd9f59a0d040e60a560d57532d4c1a
Akil0814 <akil040814@gmail.com>
2025-01-14 16:02:41 -0800

    don

--- 25/1/13(3.cpp
+++ 25/1/13(3.cpp
@@ -1,17 +1,45 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
-#define MAXLEN 10
-
+//template <class T,int len=10>//非通用类型可以有缺省值
+template <class T,int len>
 class Array
 {
 private:
-    int items[MAXLEN];
+    T items[len];
+public:
+    Array()
+    {
+        memset(items,0,sizeof(items));//memset函数可能会有内存问题//栈上分配内存
+    }
+    ~Array()
+    {}
+
+    T& operator[](T ii)//重载操作符[] 可以修改数组中的元素
+    {
+        return items[ii];
+    }
+
+    const T& operator[](T ii) const
+    {
+        return items[ii];
+    }
 
 };
 
 int main()
 {
+    Array<int,10> aa;//可以更改《》内的数据类型// 10为数 组的大小
+    aa[0]=5;
+    aa[1]=9;
+    aa[2]=6;
+    aa[3]=1;
+    aa[4]=3;
+    for(int i=0;i<5;i++)
+    {
+        cout<<"aa"<<i<<" "<<aa[i]<<endl;
+    }
 
-    return 0; 
+    return 0;
 }
