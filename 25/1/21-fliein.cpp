927212ad38173fd7b37018810949e327b33d7155
Akil0814 <akil040814@gmail.com>
2025-01-25 23:23:03 -0800

    d

---
+++ 25/1/21-fliein.cpp
@@ -0,0 +1,34 @@
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string str="W:\\Coding\\code_text_output\\21.txt";//可以用string
+
+    ifstream fin(str);//输入流//不会创建文件//fin为对象名，可以任取
+    fin.open(str,ios::in);
+    //输入模式只有一种
+    //ios::in  缺省值
+    //ifstream fin(str)；
+    //ifstream fin(str，ios::in)；
+
+
+    //判断文件打开是否成功
+    //失败原因：1）目录不存在 2）文件不存在 3）没有权限，Linux平台很常见
+    if(fin.is_open()==false)
+    {
+        cout<<"flie "<<str<<"faled to open"<<endl;
+        return 0;
+    }
+
+    //读取文件内容
+    //文件文本一般以行的方式组织数据
+    getline()
+
+
+    fin.close();
+
+    return 0;
+}
---
+++ Math3/1.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+double x1=0;//for one side of interval
+double x2=0;//other side of interval
+
+double x1s=0;//x1 save from last position
+double x2s=0;//x2 save from last position
+
+double mid=0;//mid poit of x1 and x2
+
+//#define Func1 sin(x1)-pow(x1,2)+x1
+//#define Func2 sin(x2)-pow(x2,2)+x2
+
+#define Func1 pow(x1,3)-x1+1
+#define Func2 pow(x2,3)-x2+1
+
+
+
+using namespace std;
+
+void Search(double f1,double f2)
+{
+    mid=(x1+x2)/2;//mid point of x1 and x2
+    x2=mid;//move x2 to mid point
+    f1=Func1;//find y val for curent x1
+    f2=Func2;//find y val for curent x2
+
+    if(f1>0&&f2<0 || f1<0&&f2>0)//if one is biger than zero and one is not
+    {
+        x2s=x2;//update saveed point
+        Search(f1,f2);
+    }
+
+    else if(f1>0&&f2>0 || f1<0&&f2<0)//if both is on one side
+    {
+        x2=x2s;//revers saved point
+        x1=mid;//reset x1 as mid point
+        x1s=x1;//update saveed point
+        Search(f1,f2);
+    }
+    else
+    {
+        cout<<"Find the point"<<endl;
+        cout<<"x= "<<x1<<endl;
+
+        return;
+    }
+    
+}
+
+int main()
+{
+    cout<<"Enter x1:";
+    cin>>x1;
+    cout<<"Enter x2:";
+    cin>>x2;
+
+    double f1=Func1;//y val of x1
+    double f2=Func2;//y val of x2
+
+    x1s=x1; x2s=x2;//save x1 x2 data
+
+    if( f1>0&&f2<0 || f1<0&&f2>0)//if answer is in the interval
+    {
+        cout<<"Is in this interval"<<endl;
+        Search(f1,f2);
+    }
+    else
+        cout<<"Not in this interval"<<endl;
+
+    return 0;
+}
