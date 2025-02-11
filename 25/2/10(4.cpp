3134a9273839566fbae54c0cf57dcc84d96eaa3b
Akil0814 <akil040814@gmail.com>
2025-02-11 15:21:28 -0800

    2/11

--- 25/2/10(4.cpp
+++ 25/2/10(4.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main()
 {
-    
+    //1 map();//创建一个空的map容器
+    map<int,string>m1;
+
+    //2 map(initializer_lits<pair<K,V>>il);//使用统一初始化列表
+    map<int,string> m2({{20,"akil"},{20,"mai"}})
+
+
     return 0;
 }
