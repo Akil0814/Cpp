463e9bd8d79ac66be5dc068b61178cefb5f2eaaf
Akil0814 <akil040814@gmail.com>
2025-02-12 20:48:58 -0800

    2/12

--- CS1B/2/6_2.cpp
+++ CS1B/2/6_2.cpp
@@ -7,29 +7,44 @@ which has the largest sum and return its sum.
 #include<iostream>
 using namespace std;
 
-const int num=5;
+const int n=5;
 
-int findMaxSubarray(int arr[num])
+int findMaxSubarray(int arr[n])
 {
-    int sumOfMaxSub=arr[0];
-    for(int i=0;i<num;i++)
+    int max=arr[0];
+    int SubArr[n]={0};
+
+    for(int start=0;start<n;start++)
     {
-        for(int j=0;j<i+1;j++)
+        for(int end=start;end<n;end++)
         {
-            int tmp=0;
-            tmp=tmp+arr[j];
-            if(tmp>sumOfMaxSub)
-                sumOfMaxSub=tmp;
+
+            int sum=0;
+            for(int i=start;i<=end;i++)
+                sum+=arr[i];
+            if(sum>max)
+            {
+                max=sum;
+                int z=0;
+                for(int j=start;j<=end;j++)
+                {
+                    SubArr[z]=arr[j];
+                    z++;
+                }
+            }
         }
-        return sumOfMaxSub;
     }
 
-    return -1;
+    for(int& iter:SubArr)
+        cout<<iter<<" ";
+    cout<<endl;
+
+    return max;
 }
 
 int main()
 {
-    int arr[num]={1,-2,4,6,-9};
+    int arr[n]={1,-2,4,6,-9};
 
     cout<<findMaxSubarray(arr);
 
