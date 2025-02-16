b5af4f77d022d4cf014cb2753c3d88641328b6f9
Akil0814 <akil040814@gmail.com>
2025-02-15 21:46:14 -0800

    2/15

--- 25/1/14.cpp
+++ 25/1/14.cpp
@@ -2,31 +2,6 @@
 #include<string.h>
 using namespace std;
 
-//template <class T,int len=10>//非通用类型可以有缺省值
-template <class T,int len>
-class Array
-{
-private:
-    T items[len];
-public:
-    Array()
-    {
-        //memset(items,0,sizeof(items));//memset函数可能会有内存问题//栈上分配内存
-    }
-    ~Array()
-    {}
-
-    T& operator[](T ii)//重载操作符[] 可以修改数组中的元素
-    {
-        return items[ii];
-    }
-
-    const T& operator[](T ii) const
-    {
-        return items[ii];
-    }
-
-};
 
 template <class T>
 class Vector
@@ -79,7 +54,6 @@ public:
 
 int main()
 {
-    //Array<int,10> aa;//可以更改《》内的数据类型//10为数 组的大小//在栈上分配内存 容易维护 执行速度快 适合小型数组
     Vector<string> aa(1); //创建模板类Vector的对象//更通用 可以自动扩展
 
     aa[0]=5; aa[1]=9; aa[2]=6; aa[3]=1; aa[4]=3;
--- 25/2/14.cpp
+++ 25/2/14.cpp
@@ -3,10 +3,42 @@
 using namespace std;
 
 template<class K, class V>
-using umap= std::unordered_map<K,V>
+using umap= std::unordered_map<K,V>;
 
 int main()
 {
+    //1 umap();//创建一个空的map容器
+    umap<int,string>m1;
+
+//---------------------------------------------------------------------------------------------
+    //2 umap(initializer_lits<pair<K,V>>il);//使用统一初始化列表
+    //umap<int,string> m2={{20,"akil"},{19,"02"},{21,"mai"},{500,"CC"},{22,"saber"}};
+    umap<int,string> m2({{20,"akil"},{19,"02"},{21,"mai"},{500,"CC"},{22,"saber"}});
+    //输出：19 02 20 akil 21 mai 22 saber 500 CC //会对数据自动排序
+    cout<<"m2:  ";
+    for(auto& val:m2)
+        cout<<val.first<<" "<<val.second<<" ";
+    cout<<endl;
+
+//---------------------------------------------------------------------------------------------
+    umap<int,string>m3=m2;//拷贝构造函数
+    cout<<"m3:  ";
+    for(auto& val:m3)
+        cout<<val.first<<","<<val.second<<" ";
+    cout<<endl;
+
+//---------------------------------------------------------------------------------------------
+    //4 umap(iterator first,iterator last);//用迭代器创建map容器
+    auto first=m3.begin();
+    first++;
+    auto last=m3.end();
+    last--;
+    umap<int,string>m4(first,last);
+    cout<<"m4:  ";
+    for(auto& val:m4)
+        cout<<val.first<<","<<val.second<<" ";
+    cout<<endl;
+
 
     return 0;
 }
