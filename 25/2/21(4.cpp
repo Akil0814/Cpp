14fbefa8f8ae0c28a67c4d5fa9f1fd332c2085c4
Akil0814 <akil040814@gmail.com>
2025-02-21 20:02:21 -0800

    2/21

---
+++ 25/2/21(4.cpp
@@ -0,0 +1,33 @@
+//随机存取
+//移动文件指针后的读取操作
+#include <iostream>
+#include <fstream>
+using  namespace std;
+int main()
+{
+    string filename = R"(W:\Coding\code_text_output\21.txt)";
+
+    fstream fs;
+    fs.open(filename,ios::in|ios::out);
+
+    if(fs.is_open()==false)
+    {
+        cout<<"打开文件"<<filename<<"失败"<<endl;
+        return 0;
+    }
+
+    fs.seekg(3);//会跳过前几个字节读取
+
+    cout<<"文件位置指针："<<fs.tellg()<<endl;
+    string buffer;
+    while(fs>>buffer)
+    {
+        cout<<"文件位置指针："<<fs.tellg()<<endl;
+        cout<<buffer<<endl;
+    }
+
+
+    fs.close();
+
+    return 0;
+}
---
+++ 25/2/21(5.cpp
@@ -0,0 +1,29 @@
+//随机存取
+//移动文件指针后的读取操作
+#include <iostream>
+#include <fstream>
+using  namespace std;
+int main()
+{
+    string filename = R"(W:\Coding\code_text_output\21.txt)";
+
+    fstream fs;
+    fs.open(filename,ios::in|ios::out);
+
+    if(fs.is_open()==false)
+    {
+        cout<<"打开文件"<<filename<<"失败"<<endl;
+        return 0;
+    }
+
+    fs.seekg(4);//会跳过前几个字节写入 有可能覆盖换行符
+
+    fs<<"CC";
+
+    cout<<"文件位置指针："<<fs.tellg()<<endl;
+
+
+    fs.close();
+
+    return 0;
+}
