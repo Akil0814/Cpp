dfb3bbab63a3970d460ba9c1a1412a7de0f7f4f6
Akil0814 <akil040814@gmail.com>
2025-02-26 20:29:03 -0800

    2/26

--- School/CS1B/2/24.cpp
+++ School/CS1B/2/24.cpp
@@ -4,6 +4,31 @@
 using namespace std;
 
 
+int findRepeatingNumbers(const int* arr,int size)
+{
+    int maxNum=*arr;
+
+    for(int i=0;i<size;i++)
+    {
+        if(*(arr+i)>maxNum)
+        {
+            maxNum=*(arr+i);
+        }
+    }
+
+    int* v=new int[maxNum+1];
+
+    for(int i=0;i<size;i++)
+    {
+        if(*(v+*(arr+i))==1)
+            return *(arr+i);
+
+        *(v+*(arr+i))=1;
+    }
+
+    return -1;
+}
+
 int findRepeatingNumbers(const vector<int>&arr)
 {
     vector<int>v;
@@ -44,8 +69,11 @@ int findRepeatingNumbers_1(const vector<int>& arr) {
 
 int main()
 {
-    vector<int>arr={3,0,1,5,9,3,8};
-        cout<<findRepeatingNumbers(arr);
+    //vector<int>arr={3,0,1,5,9,3,8};
+    int arr1[7]={3,0,1,5,9,3,8};
+
+    //cout<<findRepeatingNumbers(arr)<<endl;
+    cout<<findRepeatingNumbers(arr1,sizeof(arr1)/4);
 
     return 0;
 }
