5dee0bb6f3c5eeb22285aaa306a909b3c0862227
Akil0814 <akil040814@gmail.com>
2025-03-01 22:08:54 -0800

    3/1

--- 25/3/1(2.cpp
+++ 25/3/1(2.cpp
@@ -1,24 +1,24 @@
-//可变参数
+// 可变参数
 #include <iostream>
 #include <thread>
 using namespace std;
 
-void print()//没有终止函数 代码会报错
+void print() // 没有终止函数 代码会报错
 {
-    cout<<"递归终止";
+    cout << "递归终止";
 }
 
-template<typename T,typename...Args>
-void print(T arg,Args... args)
+template <typename T, typename... Args>
+void print(T arg, Args... args)
 {
-    cout<<"参数："<<arg<<endl;//显示本次展开的参数
-    cout<<sizeof...(args)<<"未展开"<<endl;
+    cout << "参数：" << arg << endl; // 显示本次展开的参数
+    cout << sizeof...(args) << "未展开" << endl;
     print(args...);
 }
 
 int main()
 {
-    print("akil",20,"mai",22);
+    print("akil", 20, "mai", 22);
 
     return 0;
 }
---
+++ 25/3/1(3.cpp
@@ -0,0 +1,9 @@
+//时间操作
+#include<chrono>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
