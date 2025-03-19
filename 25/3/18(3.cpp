39de06a45d8325e0ef1c91ff08c92b4988eac18a
Akil0814 <akil040814@gmail.com>
2025-03-18 20:21:01 -0700

    3/18

--- 25/3/18(3.cpp
+++ 25/3/18(3.cpp
@@ -1,18 +1,28 @@
 // 线程
+//call_once
+//在线程环境中保证函数只被调用一次
 #include <iostream>
 #include <thread>
 #include <windows.h>
+#include <mutex>//std::once_flag和std::call_once()函数需要包含这个头文件
 using namespace std;
+
+once_flag onceflag;//定义一个onec_flag类型的全局变量
 
 void func(int num, const string &str)
 {
-    cout<<"子线程: "<<this_thread::get_id()<<endl;//获取线程id
 
     for (int i = 1; i <= 3; i++)
     {
         cout << "第" << i << " 次,num=" << num << " neam=" << str << endl;
         Sleep(1000);
     }
+}
+
+//在线程中 只像调用一次
+void once_func(const int num, const string& str)
+{
+    cout<<"once_func num: "<<num<<" str:"<<str<<endl;
 }
 
 
@@ -22,16 +32,8 @@ int main()
     thread t1(func, 1, "akill");
     thread t2(func,2,"maiii");
 
-    cout<<"主线程: "<<this_thread::get_id()<<endl;//获取线程id 在主线程中 会显示主线程id
-    cout<<"线程 t1: "<<t1.get_id()<<endl;//获取子线程id
-    cout<<"线程 t2: "<<t2.get_id()<<endl;
-
-    thread t3=move(t2);//移动构造
-
     t1.join();
-    //t2.join();//t2被转移资源后不再代表线程 join会报错
-    t3.join();
-
+    t2.join();
 
     return 0;
 }
