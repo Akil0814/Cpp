cbd9b94b978289a2a87b864a4ec9e3dba153f67c
Akil0814 <akil040814@gmail.com>
2025-03-21 18:59:43 -0700

    3/20

---
+++ 25/3/20.cpp
@@ -0,0 +1,36 @@
+// 线程
+// 线程安全
+
+//同一进程的多个线程共享该进程中全部的系统资源
+//多线程访问同一共享资源时会产生冲突
+
+#include <iostream>
+#include <thread>
+using namespace std;
+
+int a=0;//共享的全局变量
+
+void func()
+{
+    for (int i = 1; i <= 100000; i++)
+    {
+        a++;
+    }
+}
+
+int main()
+{
+
+    //func();
+    //func();//最终结果200000
+
+    thread t1(func);//用普通函数创建线程
+    thread t2(func);//最终结果100000+ 不到200000
+
+    t1.join();
+    t2.join();
+
+    cout<<"a= "<<a<<endl;
+
+    return 0;
+}
