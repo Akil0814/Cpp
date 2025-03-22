40881ece81cef46f86d1c482c1e54436bb533624
Akil0814 <akil040814@gmail.com>
2025-03-21 22:14:08 -0700

    3/21

--- 25/3/21.cpp
+++ 25/3/21.cpp
@@ -1,5 +1,5 @@
 // 线程
-// 线程安全
+// 线程安全 volatile关键字
 //同一进程的多个线程共享该进程中全部的系统资源
 //多线程访问同一共享资源时会产生冲突
 
@@ -7,7 +7,7 @@
 #include <thread>
 using namespace std;
 
-int a=0;//共享的全局变量
+volatile int a=0;//共享的全局变量
 
 void func()
 {
