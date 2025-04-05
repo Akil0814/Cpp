53e907ab1247b00ff733ccdb5e779c60af525f22
Akil0814 <akil040814@gmail.com>
2025-04-04 18:06:11 -0700

    4/4

--- 25/4/2.cpp
+++ 25/4/2.cpp
@@ -20,15 +20,28 @@ struct CC	// 类中有普通成员函数。
 	}
 };
 
+template<typename Fn,typename ...Args>
+void show(Fn fn,Args...args)
+{
+    cout<<"准备中..."<<endl;
+    //function<>=bind(fn,args);//不知道传进来的是什么样的可调用对象
+    auto f=bind(fn,args...);
+    f();
+
+    cout<<"完成..."<<endl;
+}
+
 int main()
 {
-
+    show(show0);
+    show(show1,"aaaaa");
+    CC cc;
+    show(&CC::show2,&cc,3,"bbbbb");
 
     // thread t1(show0);
     // thread t2(show1,"aaaaa");
     // CC cc;
     // thread t3(&CC::show2,&cc,3,"bbbbb");
-
     // t1.join();
     // t2.join();
     // t3.join();
