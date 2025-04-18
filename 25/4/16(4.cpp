e0db938021a34f90c58f38c7599262c3f2fb5acb
Akil0814 <akil040814@gmail.com>
2025-04-17 20:05:57 -0700

    4/17

--- 25/4/16(4.cpp
+++ 25/4/16(4.cpp
@@ -20,10 +20,25 @@ int main()
 {
     Logger a;
 
-    //void* fptr=a.log();
+    //auto fptr=a.log();
+    //execute(*fptr);
+    //void(const string&);//你可能以为的签名
+    //void(Logger*, const string&);//实际上的真实签名
+    //不能直接使用 缺少一个Logger* this作为参数
+
+
+
     //execute(a.log());
     //execute(Logger::log);
-    //execute(*fptr);
+
+    // 方法 1：使用 std::bind 把成员函数和对象绑定起来
+    auto bound_cb = bind(&Logger::log, &logger, placeholders::_1);
+    execute(bound_cb);
+
+       // 方法 2：使用 lambda 封装成员函数调用
+    execute([&logger](const string& msg) {
+        logger.log(msg);
+    });
 
     return 0;
 }
