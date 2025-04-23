313d33c5f5821760c02b798261ff30cdc1c8372d
Akil0814 <akil040814@gmail.com>
2025-04-23 15:32:51 -0700

    4/23

---
+++ 25/4/23.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct MyAdder {
+    int x;
+    MyAdder(int val) : x(val) {}
+
+    int operator()(int a) {
+        return a + x;
+    }
+};
+int main()
+{
+    //MyAdder f;
+
+
+    cout << MyAdder(100)(23) << endl;  // 输出 123
+
+
+    return 0;
+}
