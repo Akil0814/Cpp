bf7126ee00b8d06138d485e05847ae17fbac1383
Akil0814 <akil040814@gmail.com>
2025-04-25 18:58:09 -0700

    4/25

--- 25/4/24.cpp
+++ 25/4/24.cpp
@@ -1,8 +1,26 @@
 //给你一个整数数组 nums，判断是否存在一个连续子数组，它的和为 0。
 
+#include <unordered_set>
+#include <vector>
+using namespace std;
 
-int main()
-{
+class Solution {
+public:
+    bool hasZeroSumSubarray(vector<int>& nums) {
+        unordered_set<int> seen;
+        int prefixSum = 0;
 
-    return 0;
-}
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (prefixSum == 0 || seen.count(prefixSum)) {
+                return true;
+            }
+
+            seen.insert(prefixSum);
+        }
+
+        return false;
+    }
+};
+
