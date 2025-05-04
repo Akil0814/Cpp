6b6e96509027012e8714f8fe4d88018f4eab3f4c
Akil0814 <akil040814@gmail.com>
2025-05-03 21:07:50 -0700

    5/3

--- 24/11/21(4.cpp
+++ 24/11/21(4.cpp
@@ -1,31 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//函数模板支持重载
-//函数模板可以有包含非通用类型的参数
-template<typename T1,typename T2>
-void func(T1 a,T2 b)
+// 函数模板支持重载
+// 函数模板可以有包含非通用类型的参数
+template <typename T1, typename T2>
+void func(T1 a, T2 b)
 {
-    cout<<"func(T1 a,T2 b)"<<endl;
+    cout << "func(T1 a,T2 b)" << endl;
 }
 
-template<typename T1,typename T2>
-void func(T1 a,T2 b,int c)
+template <typename T1, typename T2>
+void func(T1 a, T2 b, int c)
 {
-    cout<<"func(T1 a,T2 b,int c)"<<endl;
+    cout << "func(T1 a,T2 b,int c)" << endl;
 }
 
-template<typename T1>
+template <typename T1>
 void func(T1 a)
 {
-    cout<<"func(T1 a)"<<endl;
+    cout << "func(T1 a)" << endl;
 }
 
 int main()
 {
     func(1);
-    func(1,2);
-    func(1,2,4);
+    func(1, 2);
+    func(1, 2, 4);
 
     return 0;
 }
