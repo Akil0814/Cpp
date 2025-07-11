7cbb0f52b7c65620e85d84205a88194beb62ca1d
Akil0814 <akil040814@gmail.com>
2025-07-10 20:17:47 -0700

    7/10

--- Sort/Quick.cpp
+++ Sort/Quick.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void SortArr()
+void QuickSort(int* first, int* last)
 {
+    if (first >= last)
+        return;
+
+    int pivot=*first;
+    
 
 }
 
@@ -11,7 +16,7 @@ int main()
     const int len=15;
     int arr[len]={8,2,9,4,3,5,11,43,10,65,44,95,70,2,15};
 
-    SortArr(arr,len);
+    QuickSort(&arr[0],&arr[14]);
 
     for(int i:arr)
         cout<<i<<" ";
