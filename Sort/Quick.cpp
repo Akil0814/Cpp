98037d32d08f10a8ce0655b19a512825efffd483
Akil0814 <akil040814@gmail.com>
2025-07-11 21:36:24 -0700

    7/11

--- Sort/Quick.cpp
+++ Sort/Quick.cpp
@@ -1,13 +1,58 @@
 #include<iostream>
 using namespace std;
 
-void QuickSort(int* first, int* last)
+void QuickSort_hoare(int* first, int* last)//指针对撞
+{
+    if (first >= last)
+        return;
+    int pivot=*first;
+
+    int* low=first;
+    int* high=last;
+
+    while(pivot>*high)
+        --high;
+
+    while(pivot<*low)
+        ++low;
+
+    swap(*low,*high);
+    swap(*first,*low);
+
+
+    QuickSort_hoare(first,low-1);
+    QuickSort_hoare(low+1,last);
+
+}
+
+
+
+void QuickSort_hole(int* first, int* last)//挖坑填数
 {
     if (first >= last)
         return;
 
     int pivot=*first;
-    
+    int* low=first;
+    int* high=last;
+
+    while(low<high)
+    {
+        while(low<high&& *high>=pivot)
+        --high;
+
+        *low=*high;
+
+        while(low<high&&*low<=pivot)
+        ++low;
+
+        *high=*low;
+    }
+
+    *low=pivot;
+
+    QuickSort_hole(first,low-1);
+    QuickSort_hole(low+1,last);
 
 }
 
@@ -16,7 +61,7 @@ int main()
     const int len=15;
     int arr[len]={8,2,9,4,3,5,11,43,10,65,44,95,70,2,15};
 
-    QuickSort(&arr[0],&arr[14]);
+    QuickSort_hoare(&arr[0],&arr[14]);
 
     for(int i:arr)
         cout<<i<<" ";
