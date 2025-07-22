7b1641d14598d8df2b737b98547373a78706ea48
Akil0814 <akil040814@gmail.com>
2025-07-21 19:52:01 -0700

    7/21

--- 25/7/10.cpp
+++ 25/7/10.cpp
@@ -1,7 +1,8 @@
+#include <deque>
 #include <iostream>
 using namespace std;
-//二叉树
-//创建 深度优先 广度优先
+// 二叉树
+// 创建 深度优先 广度优先
 
 struct Tree
 {
@@ -10,52 +11,80 @@ struct Tree
     Tree *right = nullptr;
 };
 
-void create_tree(Tree *top, int num)//num 代表要创建几层
+void create_tree_by_level(Tree *top, int level,int& start_num) // num 代表要创建几层
 {
-    if (num <= 0)
+    if (level <= 0)
         return;
 
-    static int count = 1;
-    top->data = count++;
+    top->data =start_num++;
 
-    if (num > 1)//当num=1时代表已经到达底层了 不需要继续创建
+    if (level > 1) // 当num=1时代表已经到达底层了 不需要继续创建
     {
         top->left = new Tree;
         top->right = new Tree;
-        create_tree(top->left, num - 1);
-        create_tree(top->right, num - 1);
+        create_tree_by_level(top->left, level - 1,start_num);
+        create_tree_by_level(top->right, level - 1,start_num);
     }
+}
+
+void creat_tree_by_count(Tree* top, int count,int& start_num)
+{
+    
+}
+
+void destroy_tree(Tree* top)
+{
+    if (!top) return;
+    destroy_tree(top->left);
+    destroy_tree(top->right);
+    delete top;
 }
 
 void depth_first(Tree *top)
 {
-    if (top == nullptr)//检测空指针
+    if (top == nullptr) // 检测空指针
         return;
 
-    //访问根节点 → 遍历左子树 → 遍历右子树
+    // 访问根节点 → 遍历左子树 → 遍历右子树
     cout << top->data << " ";
     depth_first(top->left);
     depth_first(top->right);
-    cout << endl;
 }
 
-
-void breadth_first(Tree *top)
+void breadth_first(Tree* top)
 {
-    if (top == nullptr)//检测空指针
+    if (top == nullptr)
         return;
-    cout << top->data << " ";
-    while(true)
+
+    //创建队列
+    deque<Tree*> tree_deque;
+    tree_deque.push_back(top);//将头节点加入
+
+    while (!tree_deque.empty())
     {
+        Tree* curr = tree_deque.front();
+        tree_deque.pop_front();
+
+        std::cout << curr->data << " ";//输出队头元素
+
+        if (curr->left)
+            tree_deque.push_back(curr->left);
+        if (curr->right)
+            tree_deque.push_back(curr->right);
     }
 }
-
 
 int main()
 {
     Tree *top = new Tree;
-    create_tree(top, 4);
-    print_tree(top);
+    int s_num=1;
+    create_tree_by_level(top, 4,s_num);
+
+    depth_first(top);
+    cout << endl;
+    breadth_first(top);
+
+    destroy_tree(top);
 
     return 0;
 }
