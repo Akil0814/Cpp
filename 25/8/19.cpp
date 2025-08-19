0dcb6d787879a85f098e6e4f5fed0fe8e5746c6b
Akil0814 <akil040814@gmail.com>
2025-08-19 14:35:06 -0700

    8/19

---
+++ 25/8/19.cpp
@@ -0,0 +1,27 @@
+//leetcode 20
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(string s)
+{
+    if(s.empty())
+        return true;
+
+    vector<pair<char,char>> p;
+
+    for(auto iter= s.begin();iter!=s.end();iter++)
+    {
+
+    }
+
+   return false;
+}
+
+int main()
+{
+    string str="";
+    std::cout<<isValid(str);
+
+    return 0;
+}
