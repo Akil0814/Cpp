260b0b9709131f9f5b839131c43000b864d01499
Akil0814 <akil040814@gmail.com>
2025-09-03 18:22:55 -0700

    9/3

---
+++ 25/9/3.cpp
@@ -0,0 +1,28 @@
+// leetcode 70
+#include<iostream>
+#include<unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+int climbStairs(int n)
+{
+    if(n<=0)
+        return 0;
+
+
+    while()
+    {
+
+    }
+
+}
+
+int main()
+{
+    int x=0;
+
+    std::cout<<climbStairs(x)<<std::endl;
+
+    return 0;
+}
