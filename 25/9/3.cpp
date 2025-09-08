53642c5fd7cbce1ee63214a341fe0d5969ce359e
Akil0814 <akil040814@gmail.com>
2025-09-08 14:36:18 -0700

    9/8

--- 25/9/3.cpp
+++ 25/9/3.cpp
@@ -5,22 +5,44 @@
 #include <vector>
 using namespace std;
 
-int climbStairs(int n)
+int climbStairs(int n,bool my)
 {
     if(n<=0)
         return 0;
 
+    vector<int> dp={1,2};
+    int index=2;
 
-    while()
+    while(n>dp.size())
     {
-
+        int tmp=dp[index-1]+dp[index-2];
+        dp.push_back(tmp);
+        index++;
     }
+
+    return dp[n-1];
 
 }
 
+int climbStairs(int n)
+{
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+
 int main()
 {
-    int x=0;
+    int x=5;
 
     std::cout<<climbStairs(x)<<std::endl;
 
---
+++ 25/9/8.cpp
@@ -0,0 +1,18 @@
+// leetcode 88
+#include<iostream>
+#include<unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+{
+    nums1.shrink_to_fit(m);
+    nums2.shrink_to_fit(n);
+    for(nums1)
+}
+int main()
+{
+
+    return 0;
+}
