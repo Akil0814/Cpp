25bf13a80dba938360843128a269f97505ec4678
Akil0814 <akil040814@gmail.com>
2025-12-13 22:10:02 -0800

    12/13

--- 25/12/8.cpp
+++ 25/12/8.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 int main()
 {
-    int a[5]={1,2,3,4,5};//== *(p+2)== *(2+p)
-    int* p=a;
+    int a[5]={1,2,3,4,5};
+    //int* p=a;
     std::cout<<3[a]<<std::endl;
-    std::cout<<a[3]<<std::endl;
-    std::cout<<a<<std::endl;
-    std::cout<<a+3<<std::endl;
+    
+    //std::cout<<a[3]<<std::endl;
+    //std::cout<<*(a+3)<<std::endl;
+    //std::cout<<*(3+a)<<std::endl;
+
 
 
 
